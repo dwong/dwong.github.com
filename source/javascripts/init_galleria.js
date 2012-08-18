9d0eb3a6d4de787c2dbbdf3fefd8c7619d805a73
Derek Wong <derekpublic@gmail.com>
2012-08-17 22:53:28 -0700

    Only load galleria for pages that have applicable objects

--- source/javascripts/init_galleria.js
+++ source/javascripts/init_galleria.js
@@ -1,9 +1,15 @@
-jQuery('.galleria').galleria({
-  width: 700,
-  height: 500
-});
 
-Galleria.loadTheme('/javascripts/galleria/themes/classic/galleria.classic.min.js');
-Galleria.run('.galleria', {
-  lightbox: true
+jQuery(document).ready(function() {
+if (jQuery('.galleria')[0])
+{
+  jQuery('.galleria').galleria({
+    width: 700,
+    height: 500
+  });
+
+  Galleria.loadTheme('/javascripts/galleria/themes/classic/galleria.classic.min.js');
+  Galleria.run('.galleria', {
+    lightbox: true
+  });
+}
 });
