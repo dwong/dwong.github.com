aff0fae4f7f12112536a1a950e0da1d72df99953
Derek Wong <derekpublic@gmail.com>
2012-09-30 14:58:55 -0700

    Use Google and CDNJS instead of combining Javascripts

---
+++ source/javascripts/init_jquery.js
@@ -0,0 +1,1 @@
+jQuery.noConflict();jQuery(document).ready(function(){var a=jQuery.cookie("sidebar_collapsed",{raw:true});if(a==="1"){$("body").addClass("collapse-sidebar");}else{$("body").removeClass("collapse-sidebar");}});
