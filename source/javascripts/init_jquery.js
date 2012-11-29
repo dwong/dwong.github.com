0a1f3dc2a58d3844f6b5508664ae27ad2ec0a754
Derek Wong <derekpublic@gmail.com>
2012-11-28 21:00:30 -0800

    Revert default state of sidebar to displayed

--- source/javascripts/init_jquery.js
+++ source/javascripts/init_jquery.js
@@ -1,1 +1,1 @@
-jQuery.noConflict();jQuery(document).ready(function(){var e=jQuery.cookie("sidebar_collapsed",{raw:true});if(e==="0"){$("body").removeClass("collapse-sidebar");}else{$("body").addClass("collapse-sidebar");}});
+Query.noConflict();jQuery(document).ready(function(){var a=jQuery.cookie("sidebar_collapsed",{raw:true});if(a==="1"){$("body").addClass("collapse-sidebar");}else{$("body").removeClass("collapse-sidebar");}});
