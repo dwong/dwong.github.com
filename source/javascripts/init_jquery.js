8805dfd8315762e7f7724c294f81f86bd9f55e09
Derek Wong <derekpublic@gmail.com>
2012-12-07 23:16:12 -0800

    Missing j in jQuery

--- source/javascripts/init_jquery.js
+++ source/javascripts/init_jquery.js
@@ -1,1 +1,1 @@
-Query.noConflict();jQuery(document).ready(function(){var a=jQuery.cookie("sidebar_collapsed",{raw:true});if(a==="1"){$("body").addClass("collapse-sidebar");}else{$("body").removeClass("collapse-sidebar");}});
+jQuery.noConflict();jQuery(document).ready(function(){var a=jQuery.cookie("sidebar_collapsed",{raw:true});if(a==="1"){$("body").addClass("collapse-sidebar");}else{$("body").removeClass("collapse-sidebar");}});
