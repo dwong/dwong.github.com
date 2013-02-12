525a361106e3e00b350ef98266e6eecc6dd2d580
Derek Wong <derekpublic@gmail.com>
2013-02-11 23:12:37 -0800

    Site updated at 2013-02-12 07:12:37 UTC

--- javascripts/all.js
+++ javascripts/all.js
@@ -237,11 +237,15 @@ jQuery.noConflict();jQuery(document).ready(function(){var a=jQuery.cookie("sideb
 jQuery(document).ready(function(){if(jQuery('.galleria')[0]){jQuery('.galleria').galleria({height:600});Galleria.loadTheme('/javascripts/galleria/themes/gtww/galleria.gtww.js');Galleria.run('.galleria',{lightbox:true,showInfo:true})}});
 
 
-//jQuery(document).ready(function(){if(jQuery('#wookmark').length){$('#tiles li').wookmark({autoResize:true,container:$('#wookmark'),offset:2,itemWidth:200})}});
 jQuery(document).ready(function () {
   var container = jQuery('#wookmark');
     if (container.length) {
       jQuery("body").addClass("collapse-sidebar");
       jQuery('#tiles li').wookmark({autoResize:true,container:container,offset:4,itemWidth:200});
+      jQuery('#wookmark #tiles a').colorbox({
+          rel:'gal',
+          maxWidth:'80%',
+          maxHeight:'90%'
+      });
     }
 });
