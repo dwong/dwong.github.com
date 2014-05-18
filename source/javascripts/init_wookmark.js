ccef03da047da674efd6f82f70bc5e6cecc9b7e6
Derek Wong <derekpublic@gmail.com>
2014-05-17 22:32:40 -0700

    Update js libraries

--- source/javascripts/init_wookmark.js
+++ source/javascripts/init_wookmark.js
@@ -1,15 +1,22 @@
 jQuery(document).ready(function () {
     if (!jQuery('body').hasClass('no-sidebar') && jQuery('#wookmark').length) 
-      jQuery("body").addClass("collapse-sidebar");
-});
-jQuery(window).load(function () {
-  var container = jQuery('#wookmark');
+        jQuery("body").addClass("collapse-sidebar");
+
+    var container = jQuery('#wookmark');
     if (container.length) {
-      jQuery('#tiles li').wookmark({autoResize:true,container:container,offset:4,itemWidth:200});
-      jQuery('#wookmark #tiles a').colorbox({
-          rel:'gal',
-          maxWidth:'80%',
-          maxHeight:'90%'
-      });
+        var items = jQuery('#tiles li');
+        jQuery('a', items).addClass('gal').colorbox({
+            rel:'gal',
+            maxWidth:'80%',
+            maxHeight:'90%'
+        });
+        jQuery('#tiles').imagesLoaded(function(){
+            items.wookmark({
+                autoResize:true,
+		container:container,
+		offset:4,
+		itemWidth:200
+	    });
+        });
     }
 });
